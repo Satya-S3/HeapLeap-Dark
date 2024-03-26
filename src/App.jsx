7ab7a29c8f099722550e8e4ee1ca17a1bc8662f1
Satya-S3 <satyasahu644@gmail.com>
2024-03-26 09:00:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+ 
+  return (
+    <>
+      <div className="container">
+        <Header/>
+        <Footer/>
+      </div>
+    </>
+  )
+}
+
+export default App
